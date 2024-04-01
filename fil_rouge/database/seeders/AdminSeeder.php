55550e0fd9544e54ab110ddb8bb358ca53564dba
J-Maryam <132862036+J-Maryam@users.noreply.github.com>
2024-04-01 13:52:03 +0000

    admin seeder

---
+++ fil_rouge/database/seeders/AdminSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class AdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $firstUser = User::firstOrCreate(
+            [   'name' => 'Maryam',
+                'email' => 'maryam@gmail.com',
+                'password' => 'maryam@gmail.com',
+                'role' => '1'
+            ]);
+    }
+}
