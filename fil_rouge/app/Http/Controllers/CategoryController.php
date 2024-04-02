ae3d56e1de085ef1007777a6b0db8df32349fba0
J-Maryam <132862036+J-Maryam@users.noreply.github.com>
2024-04-02 11:56:37 +0000

    add categories

---
+++ fil_rouge/app/Http/Controllers/CategoryController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Categories;
+use App\Models\Category;
+use App\Models\Reserver;
+use Illuminate\Http\Request;
+
+class CategoryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+
+    public function showCategories()
+    {
+        $categories = Category::paginate(4);
+        return view('admin.categories', compact('categories'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string|max:255|unique:categories,name'
+        ]);
+
+        Category::create([
+            'name' => $request->name
+        ]);
+
+        return redirect()->back();
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+//    public function destroyCategory($id)
+//    {
+//        $category = Category::findOrFail($id);
+//        $category->delete();
+//
+//        return redirect()->back();
+//    }
+
+
+}
