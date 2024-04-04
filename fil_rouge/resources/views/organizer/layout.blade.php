0bee8085e500c896886fdda3236292622acbb3f1
J-Maryam <132862036+J-Maryam@users.noreply.github.com>
2024-04-04 01:46:52 +0000

    layout

---
+++ fil_rouge/resources/views/organizer/layout.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en" :class="isDark ? 'dark' : 'light'" x-data="{ isDark: false }">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="author" content="Mazyar">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="../style/style.css" rel="stylesheet">
+    <meta name="author" content="Mazyar">
+    <title>Movie Management Dashboard</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+
+    <script defer src="https://unpkg.com/alpinejs@3.x.x/dist/cdn.min.js"></script>
+
+</head>
+</html>
