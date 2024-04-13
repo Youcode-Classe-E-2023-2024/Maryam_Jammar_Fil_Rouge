46e949c0e7be9170ff6b3347cd23641455198b79
J-Maryam <132862036+J-Maryam@users.noreply.github.com>
2024-04-13 17:24:53 +0000

    Update dashboard.blade.php

--- fil_rouge/resources/views/organizer/dashboard.blade.php
+++ fil_rouge/resources/views/organizer/dashboard.blade.php
@@ -42,56 +42,21 @@
                 <a href="#" class="text-gray-700 dark:text-white font-semibold">Home</a>
                 <a href="#" class="hover:text-gray-700 dark:hover:text-white ">Animes</a>
             </nav>
-
-            <div
-                class="flex flex-col justify-between mt-4 bg-black/10 bg-blend-multiply rounded-3xl h-80 overflow-hidden bg-cover bg-center px-7 pt-4 pb-6 text-white"
-                style="background-image: url('images/inception.jpg');">
-                <!-- <img class="object-cover w-full h-full" src="images/inception.jpg" alt=""> -->
-                <div class="flex -space-x-1 items-center ">
-                    <img class="rounded-full w-7 h-7 shadow-lg border border-white"
-                         src="https://api.lorem.space/image/face?w=32&amp;h=32&amp;hash=zsrj8csk" alt="" srcset="">
-                    <img class="rounded-full w-7 h-7 shadow-lg border border-white"
-                         src="https://api.lorem.space/image/face?w=32&amp;h=32&amp;hash=zsrj8cck"
-                         alt="" srcset="">
-                    <img class="rounded-full w-7 h-7 shadow-lg border border-white"
-                         src="https://api.lorem.space/image/face?w=32&amp;h=32&amp;hash=zsfj8cck"
-                         alt="" srcset="">
-                    <span class="pl-4 text-xs drop-shadow-lg">+8 friends are watching</span>
-                </div>
-
-                <div class="bg-gradient-to-r from-black/30 to-transparent -mx-7 -mb-6 px-7 pb-6 pt-2">
-                    <span class="uppercase text-3xl font-semibold drop-shadow-lg ">Inception</span>
-                    <div class="text-xs text-gray-200 mt-2">
-                        <a href="#" class="">Action</a>,
-                        <a href="#" class="">Adventure</a>,
-                        <a href="#" class="">Sci-Fi</a>
-                    </div>
-                    <div class="mt-4 flex space-x-3 items-center">
-                        <a href="#" class="px-5 py-2.5 bg-red-600 hover:bg-red-700 rounded-lg text-xs inline-block">Watch</a>
-                        <a href="#" class="p-2.5 bg-gray-800/80 rounded-lg hover:bg-red-600">
-                            <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" viewBox="0 0 20 20"
-                                 fill="currentColor">
-                                <path fill-rule="evenodd"
-                                      d="M10 3a1 1 0 011 1v5h5a1 1 0 110 2h-5v5a1 1 0 11-2 0v-5H4a1 1 0 110-2h5V4a1 1 0 011-1z"
-                                      clip-rule="evenodd"/>
-                            </svg>
-                        </a>
-                    </div>
-                </div>
-            </div>
         </section>
 
         <section class="mt-9">
             <div class="flex items-center justify-between">
                 <span class="font-semibold text-gray-700 text-base dark:text-white">Categories</span>
                 <div class="flex items-center space-x-2 fill-gray-500">
-                    <svg class="h-7 w-7 rounded-full border p-1 hover:border-red-600 hover:fill-red-600 dark:fill-white dark:hover:fill-red-600"
-                         xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" onclick="showPreviousCategories()">
+                    <svg
+                        class="h-7 w-7 rounded-full border p-1 hover:border-red-600 hover:fill-red-600 dark:fill-white dark:hover:fill-red-600"
+                        xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" onclick="showPreviousCategories()">
                         <path d="M13.293 6.293L7.58 12l5.7 5.7 1.41-1.42 -4.3-4.3 4.29-4.293Z"></path>
                     </svg>
 
-                    <svg class="h-7 w-7 rounded-full border p-1 hover:border-red-600 hover:fill-red-600 dark:fill-white dark:hover:fill-red-600"
-                         xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" onclick="showNextCategories()">
+                    <svg
+                        class="h-7 w-7 rounded-full border p-1 hover:border-red-600 hover:fill-red-600 dark:fill-white dark:hover:fill-red-600"
+                        xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" onclick="showNextCategories()">
                         <path d="M10.7 17.707l5.7-5.71 -5.71-5.707L9.27 7.7l4.29 4.293 -4.3 4.29Z"></path>
                     </svg>
                 </div>
@@ -109,12 +74,13 @@
                                      fill="currentColor">
                                     <path fill-rule="evenodd"
                                           d="M10 3a1 1 0 011 1v5h5a1 1 0 110 2h-5v5a1 1 0 11-2 0v-5H4a1 1 0 110-2h5V4a1 1 0 011-1z"
-                                          clip-rule="evenodd" />
+                                          clip-rule="evenodd"/>
                                 </svg>
                             </a>
 
                             <div class="self-center flex flex-col items-center space-y-2">
-                                <span class="capitalize text-white font-medium drop-shadow-md">{{$category->name}}</span>
+                                <span
+                                    class="capitalize text-white font-medium drop-shadow-md">{{$category->name}}</span>
                             </div>
                         </div>
                     </div>
@@ -125,7 +91,7 @@
         <section class="mt-9">
             <div class="flex items-center justify-between">
                 <span class="font-semibold text-gray-700 text-base dark:text-white">My latest events</span>
-                <span class="font-semibold text-gray-700 text-base dark:text-white">View all</span>
+                <a href="/myEvents" class="underline hover:text-gray-500 text-gray-600 text-base dark:text-white">View all</a>
             </div>
 
             <div class="mt-4 grid grid-cols-2 gap-y-5 sm:grid-cols-3 gap-x-5 ">
@@ -169,12 +135,17 @@
                                             transform=" matrix(1 0 0 1 0 0) " stroke-linecap="round"/>
                                     </g>
                             </svg>
-                                <span class="capitalize  font-medium truncate">{{$event->location}}</span>
-                            </div>
+                                @if($event->event_type == 'online')
+                                    <span class="capitalize font-medium truncate">Online</span>
+                                @else
+                                    <span class="capitalize font-medium truncate">{{$event->country}}</span>,
+                                    <span class="capitalize font-medium truncate">{{$event->city}}</span>
+
+                                @endif                            </div>
                             <div>
                                 <span class="capitalize font-sm truncate">Status: {{ $event->status}}</span>
                                 {{--                                <span class="capitalize font-medium truncate">{{ \Carbon\Carbon::parse($event->date)->translatedFormat('j F Y') }}</span>--}}
-{{--                                <span class="capitalize font-medium truncate">{{ \Carbon\Carbon::parse($event->time)->format('h:i A') }}</span>--}}
+                                {{--                                <span class="capitalize font-medium truncate">{{ \Carbon\Carbon::parse($event->time)->format('h:i A') }}</span>--}}
                             </div>
 
                         </div>
