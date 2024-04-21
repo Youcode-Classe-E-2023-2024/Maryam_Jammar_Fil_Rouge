bb7b534a19f9fdab254b96554c4aca7019cb1b99
J-Maryam <132862036+J-Maryam@users.noreply.github.com>
2024-04-21 19:42:55 +0100

    afficher les infos de user connectee

--- fil_rouge/resources/views/admin/nav.blade.php
+++ fil_rouge/resources/views/admin/nav.blade.php
@@ -148,13 +148,13 @@
             <button type="button" class="dropdown-toggle flex items-center">
                 <div class="flex-shrink-0 w-10 h-10 relative">
                     <div class="p-1 bg-white rounded-full focus:outline-none focus:ring">
-                        <img class="w-8 h-8 rounded-full" src="https://laravelui.spruko.com/tailwind/ynex/build/assets/images/faces/9.jpg" alt=""/>
+                        <img class="w-8 h-8 rounded-full" src="{{ auth()->user()->picture }}" alt=""/>
                         <div class="top-0 left-7 absolute w-3 h-3 bg-lime-400 border-2 border-white rounded-full animate-ping"></div>
                         <div class="top-0 left-7 absolute w-3 h-3 bg-lime-500 border-2 border-white rounded-full"></div>
                     </div>
                 </div>
                 <div class="p-2 md:block text-left">
-                    <h2 class="text-sm font-semibold text-gray-800">John Doe</h2>
+                    <h2 class="text-sm font-semibold text-gray-800">{{ auth()->user()->name }}</h2>
                     <p class="text-xs text-gray-500">Administrator</p>
                 </div>
             </button>
