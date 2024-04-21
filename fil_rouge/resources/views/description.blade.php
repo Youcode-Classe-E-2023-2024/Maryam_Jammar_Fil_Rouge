e794657be633e8823416221f324d26b62d006269
J-Maryam <132862036+J-Maryam@users.noreply.github.com>
2024-04-21 12:44:53 +0100

    update buttons

--- fil_rouge/resources/views/description.blade.php
+++ fil_rouge/resources/views/description.blade.php
@@ -4,6 +4,113 @@
 
 <!-- component -->
 <script src="//unpkg.com/alpinejs" defer></script>
+<style>
+    .fancy {
+        background-color: transparent;
+        border: 2px solid #000;
+        border-radius: 0;
+        box-sizing: border-box;
+        color: #fff;
+        cursor: pointer;
+        display: inline-block;
+        float: right;
+        font-weight: 700;
+        letter-spacing: 0.05em;
+        margin: 0;
+        outline: none;
+        overflow: visible;
+        padding: 1.25em 1.75em;
+        height: 4em;
+        position: relative;
+        text-align: center;
+        text-decoration: none;
+        text-transform: none;
+        transition: all 0.3s ease-in-out;
+        user-select: none;
+        font-size: 13px;
+    }
+
+    .fancy::before {
+        content: " ";
+        width: 1.5625rem;
+        height: 2px;
+        background: black;
+        top: 50%;
+        left: 1.5em;
+        position: absolute;
+        transform: translateY(-50%);
+        transform-origin: center;
+        transition: background 0.3s linear, width 0.3s linear;
+    }
+
+    .fancy .text {
+        font-size: 1.125em;
+        line-height: 1.33333em;
+        padding-left: 2em;
+        display: block;
+        text-align: left;
+        transition: all 0.3s ease-in-out;
+        text-transform: uppercase;
+        text-decoration: none;
+        color: black;
+    }
+
+    .fancy .top-key {
+        height: 2px;
+        width: 1.5625rem;
+        top: -2px;
+        left: 0.625rem;
+        position: absolute;
+        background: #e8e8e8;
+        transition: width 0.5s ease-out, left 0.3s ease-out;
+    }
+
+    .fancy .bottom-key-1 {
+        height: 2px;
+        width: 1.5625rem;
+        right: 1.875rem;
+        bottom: -2px;
+        position: absolute;
+        background: #e8e8e8;
+        transition: width 0.5s ease-out, right 0.3s ease-out;
+    }
+
+    .fancy .bottom-key-2 {
+        height: 2px;
+        width: 0.625rem;
+        right: 0.625rem;
+        bottom: -2px;
+        position: absolute;
+        background: #e8e8e8;
+        transition: width 0.5s ease-out, right 0.3s ease-out;
+    }
+
+    .fancy:hover {
+        color: white;
+        background: black;
+    }
+
+    .fancy:hover::before {
+        width: 0.9375rem;
+        background: white;
+    }
+
+    .fancy:hover .text {
+        color: white;
+        padding-left: 1.5em;
+    }
+
+    .fancy:hover .top-key {
+        left: -2px;
+        width: 0px;
+    }
+
+    .fancy:hover .bottom-key-1,
+    .fancy:hover .bottom-key-2 {
+        right: 0;
+        width: 0;
+    }
+</style>
 {{--<style>--}}
 {{--    .btn {--}}
 {{--        font-size: 1rem;--}}
@@ -557,25 +664,37 @@
                                             {{--                                                Buy Now !--}}
                                             {{--                                            </button>--}}
                                             <div class="flex justify-between">
-{{--                                                <form action="/paiement/{{$event->id}}" method="post">--}}
-{{--                                                    @csrf--}}
-                                                    <a href="/paiement/{{$event->id}}" class="card">
-                                                        <p class="img font-bold">Buy Now !</p>
-                                                        <div class="textBox">
-                                                            {{--                                                    <p class="text head">Ethereum</p>--}}
-                                                            <span class="text-lg">Get the ticket</span>
-                                                            <p class="text price">245 DH</p>
-                                                        </div>
-                                                    </a>
-{{--                                                </form>--}}
+                                                {{--                                                <form action="/paiement/{{$event->id}}" method="post">--}}
+                                                {{--                                                    @csrf--}}
+                                                <a href="/paiement/{{$event->id}}" class="card">
+                                                    <p class="img font-bold">Buy Now !</p>
+                                                    <div class="textBox">
+                                                        {{--                                                    <p class="text head">Ethereum</p>--}}
+                                                        <span class="text-lg">Get the ticket</span>
+                                                        <p class="text price">245 DH</p>
+                                                    </div>
+                                                </a>
+                                                {{--                                                </form>--}}
                                                 <div class="flex space-x-6">
                                                     @if($event->creator == auth()->id())
                                                         <a data-modal-target="progress-modal-{{$event->id}}"
-                                                           data-modal-toggle="progress-modal-{{$event->id}}" class="card2">Update</a>
+                                                           data-modal-toggle="progress-modal-{{$event->id}}"
+                                                           class="fancy">
+                                                            <span class="top-key"></span>
+                                                            <span class="text">Update</span>
+                                                            <span class="bottom-key-1"></span>
+                                                            <span class="bottom-key-2"></span>
+                                                        </a>
+
                                                         <form action="/deleteEvent/{{$event->id}}" method="post">
                                                             @csrf
                                                             @method('delete')
-                                                            <button type="submit" class="card3">Delete</button>
+                                                            <button class="fancy">
+                                                                <span class="top-key"></span>
+                                                                <span class="text">Delete</span>
+                                                                <span class="bottom-key-1"></span>
+                                                                <span class="bottom-key-2"></span>
+                                                            </button>
                                                         </form>
                                                     @endif
                                                 </div>
