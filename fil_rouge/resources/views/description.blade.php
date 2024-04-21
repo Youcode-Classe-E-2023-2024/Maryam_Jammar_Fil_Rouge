7dd45efa6816e80087f15a366500f67fa164a9da
J-Maryam <132862036+J-Maryam@users.noreply.github.com>
2024-04-21 16:17:11 +0100

    modifier la page de description

--- fil_rouge/resources/views/description.blade.php
+++ fil_rouge/resources/views/description.blade.php
@@ -457,12 +457,12 @@
             <!-- Container for demo purpose -->
             <div class="container my-24 mx-auto md:px-6">
                 <!-- Section: Design Block -->
-                <section class="mb-32">
+                <section>
                     <!-- Jumbotron -->
                     <!-- Container for demo purpose -->
-                    <div class="container my-24 mx-auto md:px-6">
+                    <div class="container pt-0 mx-auto md:px-6">
                         <!-- Section: Design Block -->
-                        <section class="mb-32 flex  flex-col ">
+                        <section class="flex flex-col ">
                             <div class="flex flex-wrap">
                                 <div class="mb-12 w-full shrink-0 grow-0 basis-auto lg:mb-0 lg:w-5/12">
                                     <div class="flex lg:py-12">
@@ -595,6 +595,7 @@
                                                     {{ \Carbon\Carbon::parse($event->date)->translatedFormat('j F Y') }}
                                                 </p>
 
+{{--                                                id="countdown{{$event->id}}"--}}
                                                 <p class="mx-auto mb-2 flex items-center md:mx-0 md:mb-2 lg:mb-0 xl:mr-20">
                                                     <svg class="mr-2" id='Clock_Circle_1_24' width='24' height='24'
                                                          viewBox='0 0 24 24' xmlns='http://www.w3.org/2000/svg'
@@ -621,7 +622,45 @@
                                                         </g>
                                                     </svg>
                                                     {{ \Carbon\Carbon::parse($event->time)->format('h:i A') }}
+
                                                 </p>
+                                                <script>
+                                                    // Date de l'événement
+                                                    var eventDate{{$event->id}} = new Date("{{ $event->date }}");
+
+                                                    // Fonction pour mettre à jour le compte à rebours
+                                                    function updateCountdown{{$event->id}}() {
+                                                        var now = new Date();
+                                                        var distance = eventDate{{$event->id}} - now;
+
+                                                        // Calcul des mois, jours, heures, minutes et secondes restants
+                                                        var months = Math.floor(distance / (1000 * 60 * 60 * 24 * 30));
+                                                        var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+                                                        var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+                                                        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+                                                        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+                                                        // Affichage du compte à rebours
+                                                        var countdownText = '';
+                                                        if (months > 0) {
+                                                            countdownText += months + " mois ";
+                                                            days -= months * 30; // Soustraire les mois pour éviter la répétition des jours
+                                                        }
+                                                        countdownText += days + " jours " + hours + "h " + minutes + "m " + seconds + "s ";
+                                                        document.getElementById("countdown{{$event->id}}").innerHTML = countdownText;
+
+                                                        // Mettre à jour le compte à rebours chaque seconde
+                                                        if (distance > 0) {
+                                                            setTimeout(updateCountdown{{$event->id}}, 1000);
+                                                        } else {
+                                                            document.getElementById("countdown{{$event->id}}").innerHTML = "L'événement est en cours !";
+                                                        }
+                                                    }
+
+                                                    // Appel initial à la fonction pour mettre à jour le compte à rebours
+                                                    updateCountdown{{$event->id}}();
+                                                </script>
+
 
                                                 <p class="mx-auto mb-2 flex items-center md:mx-0 lg:mb-0 ">
                                                     <svg class="mr-2" id='People_24' width='24' height='24'
@@ -660,13 +699,10 @@
                                                 </p>
                                             </div>
 
-                                            {{--                                            <button class="btn mt-8">--}}
-                                            {{--                                                Buy Now !--}}
-                                            {{--                                            </button>--}}
                                             <div class="flex justify-between">
                                                 {{--                                                <form action="/paiement/{{$event->id}}" method="post">--}}
                                                 {{--                                                    @csrf--}}
-                                                <a href="/paiement/{{$event->id}}" class="card">
+                                                <a href="/paiement/{{$event->id}}" class="card  mt-8">
                                                     <p class="img font-bold">Buy Now !</p>
                                                     <div class="textBox">
                                                         {{--                                                    <p class="text head">Ethereum</p>--}}
@@ -675,7 +711,7 @@
                                                     </div>
                                                 </a>
                                                 {{--                                                </form>--}}
-                                                <div class="flex space-x-6">
+                                                <div class="flex space-x-6  mt-10">
                                                     @if($event->creator == auth()->id())
                                                         <a data-modal-target="progress-modal-{{$event->id}}"
                                                            data-modal-toggle="progress-modal-{{$event->id}}"
@@ -705,11 +741,11 @@
                                     </div>
                                 </div>
                             </div>
-                            <div class="p-12">
+                            <div class="xl:pt-24 text-center">
                                 <h3 class="font-bold text-2xl">
                                     More Details
                                 </h3>
-                                <p class="text-justify">{{$event->description}}
+                                <p class="px-12 mx-auto text-normal mt-6 sm:mt-6 text-justify">{{$event->description}}
                                 </p>
                             </div>
                         </section>
@@ -723,72 +759,21 @@
         </div>
     </section>
 
-    <footer class="bg-white dark:bg-gray-900">
-        <div class="container mx-auto px-6 py-12">
-            <div class="md:-mx-3 md:flex md:items-center md:justify-between">
-                <h1 class="text-3xl font-semibold tracking-tight text-gray-800 dark:text-white md:mx-3 xl:text-4xl">
-                    Subscribe our newsletter to get update.</h1>
-
-                <div class="mt-6 shrink-0 md:mx-3 md:mt-0 md:w-auto">
-                    <a href="#"
-                       class="inline-flex w-full items-center justify-center rounded-lg bg-gray-800 px-4 py-2 text-sm text-white duration-300 hover:bg-gray-700 focus:ring focus:ring-gray-300 focus:ring-opacity-80">
-                        <span class="mx-2">Sign Up Now</span>
-
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                             stroke="currentColor" class="mx-2 h-6 w-6">
-                            <path stroke-linecap="round" stroke-linejoin="round"
-                                  d="M17.25 8.25L21 12m0 0l-3.75 3.75M21 12H3"/>
-                        </svg>
-                    </a>
-                </div>
-            </div>
+    <footer class="px-28 bg-white dark:bg-gray-900">
+        <div class="container mx-auto px-28 pb-4">
 
             <hr class="my-6 border-gray-200 dark:border-gray-700 md:my-10"/>
 
-            <div class="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+            <div class="flex justify-around">
                 <div>
                     <p class="font-semibold text-gray-800 dark:text-white">Quick Link</p>
 
                     <div class="mt-5 flex flex-col items-start space-y-2">
-                        <a href="#"
+                        <a href="/"
                            class="text-gray-600 transition-colors duration-300 hover:text-blue-500 hover:underline dark:text-gray-300 dark:hover:text-blue-400">Home</a>
                         <a href="#"
                            class="text-gray-600 transition-colors duration-300 hover:text-blue-500 hover:underline dark:text-gray-300 dark:hover:text-blue-400">Who
                             We Are</a>
-                        <a href="#"
-                           class="text-gray-600 transition-colors duration-300 hover:text-blue-500 hover:underline dark:text-gray-300 dark:hover:text-blue-400">Our
-                            Philosophy</a>
-                    </div>
-                </div>
-
-                <div>
-                    <p class="font-semibold text-gray-800 dark:text-white">Industries</p>
-
-                    <div class="mt-5 flex flex-col items-start space-y-2">
-                        <a href="#"
-                           class="text-gray-600 transition-colors duration-300 hover:text-blue-500 hover:underline dark:text-gray-300 dark:hover:text-blue-400">Retail
-                            & E-Commerce</a>
-                        <a href="#"
-                           class="text-gray-600 transition-colors duration-300 hover:text-blue-500 hover:underline dark:text-gray-300 dark:hover:text-blue-400">Information
-                            Technology</a>
-                        <a href="#"
-                           class="text-gray-600 transition-colors duration-300 hover:text-blue-500 hover:underline dark:text-gray-300 dark:hover:text-blue-400">Finance
-                            & Insurance</a>
-                    </div>
-                </div>
-
-                <div>
-                    <p class="font-semibold text-gray-800 dark:text-white">Services</p>
-
-                    <div class="mt-5 flex flex-col items-start space-y-2">
-                        <a href="#"
-                           class="text-gray-600 transition-colors duration-300 hover:text-blue-500 hover:underline dark:text-gray-300 dark:hover:text-blue-400">Translation</a>
-                        <a href="#"
-                           class="text-gray-600 transition-colors duration-300 hover:text-blue-500 hover:underline dark:text-gray-300 dark:hover:text-blue-400">Proofreading
-                            & Editing</a>
-                        <a href="#"
-                           class="text-gray-600 transition-colors duration-300 hover:text-blue-500 hover:underline dark:text-gray-300 dark:hover:text-blue-400">Content
-                            Creation</a>
                     </div>
                 </div>
 
@@ -797,10 +782,10 @@
 
                     <div class="mt-5 flex flex-col items-start space-y-2">
                         <a href="#"
-                           class="text-gray-600 transition-colors duration-300 hover:text-blue-500 hover:underline dark:text-gray-300 dark:hover:text-blue-400">+880
+                           class="text-gray-600 transition-colors duration-300 hover:text-blue-500 hover:underline dark:text-gray-300 dark:hover:text-blue-400">+212
                             768 473 4978</a>
                         <a href="#"
-                           class="text-gray-600 transition-colors duration-300 hover:text-blue-500 hover:underline dark:text-gray-300 dark:hover:text-blue-400">info@merakiui.com</a>
+                           class="text-gray-600 transition-colors duration-300 hover:text-blue-500 hover:underline dark:text-gray-300 dark:hover:text-blue-400">info@tchikita.com</a>
                     </div>
                 </div>
             </div>
@@ -808,11 +793,12 @@
             <hr class="my-6 border-gray-200 dark:border-gray-700 md:my-10"/>
 
             <div class="flex flex-col items-center justify-between sm:flex-row">
-                <a href="#"
-                   class="text-2xl font-bold text-gray-800 transition-colors duration-300 hover:text-gray-700 dark:text-white dark:hover:text-gray-300">Brand</a>
+                <a href="/"
+                   class="text-2xl font-bold text-gray-800 transition-colors duration-300 hover:text-gray-700 dark:text-white dark:hover:text-gray-300">Tchikita</a>
 
-                <p class="mt-4 text-sm text-gray-500 dark:text-gray-300 sm:mt-0">© Copyright 2021. All Rights
-                    Reserved.</p>
+                <p class="mt-4 text-sm text-gray-500 dark:text-gray-300 sm:mt-0">
+                    © {{ date('Y') }}. All Rights Reserved.
+                </p>
             </div>
         </div>
     </footer>
