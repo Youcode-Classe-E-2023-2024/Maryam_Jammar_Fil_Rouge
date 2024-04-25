27ad5aeb4ed245ea0885eef7e2254ccecd434b1b
J-Maryam <132862036+J-Maryam@users.noreply.github.com>
2024-04-25 01:25:43 +0100

    update home page

--- fil_rouge/resources/views/welcome.blade.php
+++ fil_rouge/resources/views/welcome.blade.php
@@ -65,87 +65,65 @@
                 <div x-cloak :class="[isOpen ? 'translate-x-0 opacity-100 ' : 'opacity-0 -translate-x-full']"
                      class="absolute inset-x-0 z-20 w-full bg-white px-6 py-4 shadow-md transition-all duration-300 ease-in-out dark:bg-gray-900 lg:relative lg:top-0 lg:mt-0 lg:flex lg:w-auto lg:translate-x-0 lg:items-center lg:bg-transparent lg:p-0 lg:opacity-100 lg:shadow-none lg:dark:bg-transparent">
                     <div class="lg:-px-8 flex flex-col space-y-4 lg:mt-0 lg:flex-row lg:space-y-0">
-                        <div class="flex justify-around items-center">
-                            {{--                            <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"--}}
-                            {{--                                 version="1.1" width="12" height="12" viewBox="0 0 256 256" xml:space="preserve">--}}
-
-                            {{--                                <defs>--}}
-                            {{--                                </defs>--}}
-                            {{--                                <g style="stroke: none; stroke-width: 0; stroke-dasharray: none; stroke-linecap: butt; stroke-linejoin: miter; stroke-miterlimit: 10; fill: none; fill-rule: nonzero; opacity: 1;"--}}
-                            {{--                                   transform="translate(1.4065934065934016 1.4065934065934016) scale(2.81 2.81)">--}}
-                            {{--                                    <path--}}
-                            {{--                                        d="M 45 90 c -4.418 0 -8 -3.582 -8 -8 V 8 c 0 -4.418 3.582 -8 8 -8 c 4.418 0 8 3.582 8 8 v 74 C 53 86.418 49.418 90 45 90 z"--}}
-                            {{--                                        style="stroke: none; stroke-width: 1; stroke-dasharray: none; stroke-linecap: butt; stroke-linejoin: miter; stroke-miterlimit: 10; fill: rgb(0,0,0); fill-rule: nonzero; opacity: 1;"--}}
-                            {{--                                        transform=" matrix(1 0 0 1 0 0) " stroke-linecap="round"/>--}}
-                            {{--                                    <path--}}
-                            {{--                                        d="M 82 53 H 8 c -4.418 0 -8 -3.582 -8 -8 c 0 -4.418 3.582 -8 8 -8 h 74 c 4.418 0 8 3.582 8 8 C 90 49.418 86.418 53 82 53 z"--}}
-                            {{--                                        style="stroke: none; stroke-width: 1; stroke-dasharray: none; stroke-linecap: butt; stroke-linejoin: miter; stroke-miterlimit: 10; fill: rgb(0,0,0); fill-rule: nonzero; opacity: 1;"--}}
-                            {{--                                        transform=" matrix(1 0 0 1 0 0) " stroke-linecap="round"/>--}}
-                            {{--                                </g>--}}
-                            {{--                            </svg>--}}
-
-                            <a href="/sign-up"
-                                class="transform text-gray-300 transition-colors duration-300 hover:text-gray-500 dark:text-gray-200 dark:hover:text-gray-400 lg:mx-8"
-                                type="button">
-                                Create Event
-                            </a>
-                        </div>
-                        <button data-modal-target="default-modal-register"
-                                data-modal-toggle="default-modal-register"
-                                class="transform text-gray-300 transition-colors duration-300 hover:text-gray-500 dark:text-gray-200 dark:hover:text-gray-400 lg:mx-8"
-                                type="button">
-                            Sign in
-                        </button>
-                        {{--                        <button data-modal-target="default-modal" data-modal-toggle="default-modal"--}}
-                        {{--                                class="block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"--}}
-                        {{--                                type="button">--}}
-                        {{--                            Toggle modal--}}
-                        {{--                        </button>--}}
-
-                        {{--                        <a class="transform text-gray-300 transition-colors duration-300 hover:text-blue-500 dark:text-gray-200 dark:hover:text-blue-400 lg:mx-8"--}}
-                        {{--                           href="#">Pricing</a>--}}
-                        {{--                        <a class="transform text-gray-300 transition-colors duration-300 hover:text-blue-500 dark:text-gray-200 dark:hover:text-blue-400 lg:mx-8"--}}
-                        {{--                           href="#">Contact</a>--}}
+                        <a class="transform text-gray-300 transition-colors duration-300 hover:text-blue-500 dark:text-gray-200 dark:hover:text-blue-400 lg:mx-8"
+                           href="#">About Us</a>
+                        @guest
+                            <div class="flex justify-around items-center">
+                                <a href="/sign-up"
+                                   class="transform text-gray-300 transition-colors duration-300 hover:text-gray-500 dark:text-gray-200 dark:hover:text-gray-400 lg:mx-8"
+                                   type="button">
+                                    Create Event
+                                </a>
+                            </div>
+                            <button data-modal-target="default-modal-register"
+                                    data-modal-toggle="default-modal-register"
+                                    class="transform text-gray-300 transition-colors duration-300 hover:text-gray-500 dark:text-gray-200 dark:hover:text-gray-400 lg:mx-8"
+                                    type="button">
+                                Sign in
+                            </button>
+                        @else
+                            <a class="transform text-gray-300 transition-colors duration-300 hover:text-blue-500 dark:text-gray-200 dark:hover:text-blue-400 lg:mx-8"
+                               href="#">Welcome user</a>
+                            <form method="POST" action="/logout">
+                                @csrf
+                                <button
+                                    class="transform text-gray-300 transition-colors duration-300 hover:text-blue-500 dark:text-gray-200 dark:hover:text-blue-400 lg:mx-8">
+                                    Logout
+                                </button>
+                            </form>
+                        @endguest
                     </div>
 
                     <!-- searchbar -->
-                    <div class='flex items-center justify-center '>
-                        <div class="flex rounded-full bg-[#0d1829] px-2 w-full max-w-[600px]">
-                            {{--                            <button class="self-center flex p-1 cursor-pointer bg-[#0d1829]"> <svg width="30px" height="30px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">--}}
+{{--                    <div class='flex items-center justify-center pt-4 border-none'>--}}
+{{--                        <form action="/search" method="GET" class="flex items-center  w-full">--}}
+{{--                            <div class="flex rounded-full bg-[#0d1829] px-2 w-full max-w-[600px]">--}}
+{{--                                <input--}}
+{{--                                    type="text" name="search"--}}
+{{--                                    class="w-full bg-[#0d1829] flex bg-transparent pl-2 text-[#cccccc] outline-0 border-none focus:rounded-full focus:border-none"--}}
+{{--                                    none--}}
+{{--                                    placeholder="Search by title, country, city..."--}}
+{{--                                />--}}
+{{--                                <button type="submit" class="relative bg-[#0d1829] rounded-full">--}}
+{{--                                    <svg width="25px" height="30px" viewBox="0 0 24 24" fill="none"--}}
+{{--                                         xmlns="http://www.w3.org/2000/svg">--}}
 
-                            {{--                                    <g id="SVGRepo_bgCarrier" stroke-width="0"/>--}}
+{{--                                        <g id="SVGRepo_bgCarrier" stroke-width="0"/>--}}
 
-                            {{--                                    <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"/>--}}
+{{--                                        <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"/>--}}
 
-                            {{--                                    <g id="SVGRepo_iconCarrier"> <path fill-rule="evenodd" clip-rule="evenodd" d="M11.567 9.8895C12.2495 8.90124 12.114 7.5637 11.247 6.7325C10.3679 5.88806 9.02339 5.75928 7.99998 6.4215C7.57983 6.69308 7.25013 7.0837 7.05298 7.5435C6.85867 7.99881 6.80774 8.50252 6.90698 8.9875C7.00665 9.47472 7.25054 9.92071 7.60698 10.2675C7.97021 10.6186 8.42786 10.8563 8.92398 10.9515C9.42353 11.049 9.94062 11.0001 10.413 10.8105C10.8798 10.6237 11.2812 10.3033 11.567 9.8895Z" stroke="#ff5c5c" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/> <path fill-rule="evenodd" clip-rule="evenodd" d="M12.433 17.8895C11.7504 16.9012 11.886 15.5637 12.753 14.7325C13.6321 13.8881 14.9766 13.7593 16 14.4215C16.4202 14.6931 16.7498 15.0837 16.947 15.5435C17.1413 15.9988 17.1922 16.5025 17.093 16.9875C16.9933 17.4747 16.7494 17.9207 16.393 18.2675C16.0298 18.6186 15.5721 18.8563 15.076 18.9515C14.5773 19.0481 14.0614 18.9988 13.59 18.8095C13.1222 18.6234 12.7197 18.3034 12.433 17.8895V17.8895Z" stroke="#ff5c5c" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/> <path d="M12 7.75049C11.5858 7.75049 11.25 8.08627 11.25 8.50049C11.25 8.9147 11.5858 9.25049 12 9.25049V7.75049ZM19 9.25049C19.4142 9.25049 19.75 8.9147 19.75 8.50049C19.75 8.08627 19.4142 7.75049 19 7.75049V9.25049ZM6.857 9.25049C7.27121 9.25049 7.607 8.9147 7.607 8.50049C7.607 8.08627 7.27121 7.75049 6.857 7.75049V9.25049ZM5 7.75049C4.58579 7.75049 4.25 8.08627 4.25 8.50049C4.25 8.9147 4.58579 9.25049 5 9.25049V7.75049ZM12 17.2505C12.4142 17.2505 12.75 16.9147 12.75 16.5005C12.75 16.0863 12.4142 15.7505 12 15.7505V17.2505ZM5 15.7505C4.58579 15.7505 4.25 16.0863 4.25 16.5005C4.25 16.9147 4.58579 17.2505 5 17.2505V15.7505ZM17.143 15.7505C16.7288 15.7505 16.393 16.0863 16.393 16.5005C16.393 16.9147 16.7288 17.2505 17.143 17.2505V15.7505ZM19 17.2505C19.4142 17.2505 19.75 16.9147 19.75 16.5005C19.75 16.0863 19.4142 15.7505 19 15.7505V17.2505ZM12 9.25049H19V7.75049H12V9.25049ZM6.857 7.75049H5V9.25049H6.857V7.75049ZM12 15.7505H5V17.2505H12V15.7505ZM17.143 17.2505H19V15.7505H17.143V17.2505Z" fill="#ff5c5c"/> </g>--}}
+{{--                                        <g id="SVGRepo_iconCarrier">--}}
+{{--                                            <path--}}
+{{--                                                d="M14.9536 14.9458L21 21M17 10C17 13.866 13.866 17 10 17C6.13401 17 3 13.866 3 10C3 6.13401 6.13401 3 10 3C13.866 3 17 6.13401 17 10Z"--}}
+{{--                                                stroke="#999" stroke-width="2" stroke-linecap="round"--}}
+{{--                                                stroke-linejoin="round"/>--}}
+{{--                                        </g>--}}
 
-                            {{--                                </svg></button>--}}
-
-                            <input
-                                type="text"
-                                class="w-full bg-[#0d1829] flex bg-transparent pl-2 text-[#cccccc] outline-0 border-none focus:rounded-full focus:border-"
-                                none
-                                placeholder="Search name movie or select options"
-                            />
-                            <button type="submit" class="relative bg-[#0d1829] rounded-full">
-                                <svg width="25px" height="30px" viewBox="0 0 24 24" fill="none"
-                                     xmlns="http://www.w3.org/2000/svg">
-
-                                    <g id="SVGRepo_bgCarrier" stroke-width="0"/>
-
-                                    <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"/>
-
-                                    <g id="SVGRepo_iconCarrier">
-                                        <path
-                                            d="M14.9536 14.9458L21 21M17 10C17 13.866 13.866 17 10 17C6.13401 17 3 13.866 3 10C3 6.13401 6.13401 3 10 3C13.866 3 17 6.13401 17 10Z"
-                                            stroke="#999" stroke-width="2" stroke-linecap="round"
-                                            stroke-linejoin="round"/>
-                                    </g>
-
-                                </svg>
-                            </button>
-                        </div>
-                    </div>
+{{--                                    </svg>--}}
+{{--                                </button>--}}
+{{--                            </div>--}}
+{{--                        </form>--}}
+{{--                    </div>--}}
                 </div>
             </nav>
 
@@ -236,15 +214,6 @@
                                 <input type="text" name="search"
                                        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-black focus:border-black block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-black dark:focus:border-black"
                                        placeholder="Search by title, country, city..." required>
-                                <button type="button" class="flex absolute inset-y-0 right-0 items-center pr-3">
-                                    <svg
-                                        class="w-4 h-4 text-gray-500 dark:text-gray-400 hover:text-gray-900 dark:hover:text-white"
-                                        fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                                        <path fill-rule="evenodd"
-                                              d="M7 4a3 3 0 016 0v4a3 3 0 11-6 0V4zm4 10.93A7.001 7.001 0 0017 8a1 1 0 10-2 0A5 5 0 015 8a1 1 0 00-2 0 7.001 7.001 0 006 6.93V17H6a1 1 0 100 2h8a1 1 0 100-2h-3v-2.07z"
-                                              clip-rule="evenodd"></path>
-                                    </svg>
-                                </button>
                             </div>
                             <button type="submit"
                                     class="inline-flex items-center py-2.5 px-3 ml-2 text-sm font-medium text-white bg-black rounded-lg border border-black hover:bg-gray-800 focus:ring-4 focus:outline-none focus:ring-black dark:bg-black dark:hover:bg-black dark:focus:ring-black">
