11cc4779b75d71ae3f7ff2cf572b3b58d248910b
J-Maryam <132862036+J-Maryam@users.noreply.github.com>
2024-04-26 17:10:22 +0100

    remove filter methodes

--- fil_rouge/app/Http/Controllers/HomeController.php
+++ fil_rouge/app/Http/Controllers/HomeController.php
@@ -6,7 +6,9 @@ use App\Models\Category;
 use App\Models\Event;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Http;
+
 
 class HomeController extends Controller
 {
@@ -25,6 +27,7 @@ class HomeController extends Controller
             ->where('date', '>=', now()->toDateString())
             ->orderBy('created_at', 'desc')
             ->paginate(6);
+
         $pastEvents = Event::where('status', 'Public')
             ->where(function ($query) {
                 $query->where('nbr_place', 0)
@@ -42,161 +45,7 @@ class HomeController extends Controller
         }
         return view('welcome', compact('events', 'categories', 'allCategories', 'pastEvents', 'flags', 'flagsData'));
     }
-
-    public function tomorrow()
-    {
-        $categories = Category::limit(5)->get();
-        $allCategories = Category::all();
-
-        $events = Event::where('status', 'Public')
-            ->where('date', '>', now()->toDateString())
-            ->paginate(6);
-        $pastEvents = Event::where('status', 'Public')
-            ->where(function ($query) {
-                $query->where('nbr_place', 0)
-                    ->orWhere('date', '<', now()->toDateString());
-            })
-            ->paginate(6);
-
-
-        $events = Event::whereDate('date', '=', Carbon::tomorrow()->toDateString())
-            ->where('status', 'Public')
-            ->paginate(6);
-
-        $flagsData = json_decode(file_get_contents('https://cdn.jsdelivr.net/npm/country-flag-emoji-json@2.0.0/dist/index.json'), true);
-        $flags = [];
-        $keys = array_rand($flagsData, 18);
-        foreach ($keys as $key) {
-            $flags[$key] = $flagsData[$key];
-        }
-
-        return view('events', compact('events', 'categories', 'allCategories', 'pastEvents', 'flags', 'flagsData'));
-    }
-
-    public function thisWeek()
-    {
-        $categories = Category::limit(5)->get();
-        $allCategories = Category::all();
-
-        $tomorrow = Carbon::tomorrow()->toDateString();
-
-        $events = Event::whereDate('date', $tomorrow)
-            ->where('status', 'Public')
-            ->paginate(6);
-
-        return view('events', compact('events', 'categories', 'allCategories'));
-    }
-
-
-    public function thisMonth()
-    {
-        $categories = Category::limit(5)->get();
-        $allCategories = Category::all();
-
-        // Récupérer le premier et le dernier jour du mois en cours
-        $startOfMonth = now()->startOfMonth();
-        $endOfMonth = now()->endOfMonth();
-
-        // Filtrer les événements de ce mois
-        $events = Event::whereBetween('date', [$startOfMonth, $endOfMonth])
-            ->where('status', 'Public')
-            ->paginate(6);
-
-        // Filtrer les événements passés du mois en cours
-        $pastEvents = Event::where('status', 'Public')
-            ->where(function ($query) use ($startOfMonth) {
-                $query->where('nbr_place', 0)
-                    ->orWhere('date', '<', $startOfMonth);
-            })
-            ->paginate(6);
-
-        return view('events', compact('events', 'categories', 'allCategories', 'pastEvents'));
-    }
-
-    public function thisWeekend()
-    {
-        $categories = Category::limit(5)->get();
-        $allCategories = Category::all();
-
-        $events = Event::where('status', 'Public')
-            ->where('date', '>=', now()->toDateString())
-            ->paginate(6);
-        $pastEvents = Event::where('status', 'Public')
-            ->where(function ($query) {
-                $query->where('nbr_place', 0)
-                    ->orWhere('date', '<', now()->toDateString());
-            })
-            ->paginate(6);
-
-
-        $saturday = now()->startOfWeek()->addDays(5)->toDateString();
-        $sunday = now()->startOfWeek()->addDays(6)->toDateString();
-
-        // Filtrer les événements de ce week-end
-        $events = Event::whereBetween('date', [$saturday, $sunday])
-            ->where('status', 'Public')
-            ->paginate(6);
-
-
-        return view('events', compact('events', 'categories', 'allCategories', 'pastEvents'));
-    }
-
-    public function nextWeek()
-    {
-        $categories = Category::limit(5)->get();
-        $allCategories = Category::all();
-
-        $events = Event::where('status', 'Public')
-            ->where('date', '>=', now()->toDateString())
-            ->paginate(6);
-        $pastEvents = Event::where('status', 'Public')
-            ->where(function ($query) {
-                $query->where('nbr_place', 0)
-                    ->orWhere('date', '<', now()->toDateString());
-            })
-            ->paginate(6);
-
-
-        $monday = now()->startOfWeek()->addDays(7)->toDateString();
-        $sunday = now()->startOfWeek()->addDays(13)->toDateString();
-
-        // Filtrer les événements de la semaine prochaine
-        $events = Event::whereBetween('date', [$monday, $sunday])
-            ->where('status', 'Public')
-            ->paginate(6);
-
-
-        return view('events', compact('events', 'categories', 'allCategories', 'pastEvents'));
-    }
-
-    public function nextWeekend()
-    {
-        $categories = Category::limit(5)->get();
-        $allCategories = Category::all();
-
-        $events = Event::where('status', 'Public')
-            ->where('date', '>=', now()->toDateString())
-            ->paginate(6);
-        $pastEvents = Event::where('status', 'Public')
-            ->where(function ($query) {
-                $query->where('nbr_place', 0)
-                    ->orWhere('date', '<', now()->toDateString());
-            })
-            ->paginate(6);
-
-
-        $saturday = now()->startOfWeek()->addDays(6)->addWeek()->toDateString();
-        $sunday = now()->startOfWeek()->addDays(7)->addWeek()->toDateString();
-
-        // Filtrer les événements du prochain week-end
-        $events = Event::whereBetween('date', [$saturday, $sunday])
-            ->where('status', 'Public')
-            ->paginate(6);
-
-
-        return view('events', compact('events', 'categories', 'allCategories', 'pastEvents'));
-    }
-
+    
     public function filterByCategory($categoryName)
     {
         $categories = Category::limit(5)->get();
