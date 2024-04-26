2ce324a0f77ab0d23244f79b995a5864b013f3c5
J-Maryam <132862036+J-Maryam@users.noreply.github.com>
2024-04-26 03:28:11 +0100

    update organizer dashboard

--- fil_rouge/resources/views/organizer/createEvent.blade.php
+++ fil_rouge/resources/views/organizer/createEvent.blade.php
@@ -9,42 +9,9 @@
 
     <main class=" flex-1 py-10  px-5 sm:px-10 ">
 
-        <header class=" font-bold text-lg flex items-center  gap-x-3 md:hidden mb-12 ">
-                <span class="mr-6">
-                    <svg xmlns="http://www.w3.org/2000/svg" class="h-7 w-7 text-gray-700 dark:text-white" fill="none"
-                         viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M4 6h16M4 12h16M4 18h7"/>
-                      </svg>
-                </span>
-            <svg class="h-8 w-8 fill-red-600 shrink-0" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-                <path
-                    d="M10 15.5v-7c0-.41.47-.65.8-.4l4.67 3.5c.27.2.27.6 0 .8l-4.67 3.5c-.33.25-.8.01-.8-.4Zm11.96-4.45c.58 6.26-4.64 11.48-10.9 10.9 -4.43-.41-8.12-3.85-8.9-8.23 -.26-1.42-.19-2.78.12-4.04 .14-.58.76-.9 1.31-.7v0c.47.17.75.67.63 1.16 -.2.82-.27 1.7-.19 2.61 .37 4.04 3.89 7.25 7.95 7.26 4.79.01 8.61-4.21 7.94-9.12 -.51-3.7-3.66-6.62-7.39-6.86 -.83-.06-1.63.02-2.38.2 -.49.11-.99-.16-1.16-.64v0c-.2-.56.12-1.17.69-1.31 1.79-.43 3.75-.41 5.78.37 3.56 1.35 6.15 4.62 6.5 8.4ZM5.5 4C4.67 4 4 4.67 4 5.5 4 6.33 4.67 7 5.5 7 6.33 7 7 6.33 7 5.5 7 4.67 6.33 4 5.5 4Z"></path>
-            </svg>
-            <div class="tracking-wide dark:text-white flex-1">MMovie<span class="text-red-600">.</span></div>
+        <p class="mx-auto text-center font-semibold text-md">Create your Event here! </p>
 
-            <div class="relative items-center content-center flex ml-2">
-                    <span class="text-gray-400 absolute left-4 cursor-pointer">
-                        <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24"
-                             stroke="currentColor">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                  d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path>
-                        </svg>
-                    </span>
-                <input type="text"
-                       class="text-xs ring-1 bg-transparent ring-gray-200 dark:ring-zinc-600 focus:ring-red-300 pl-10 pr-5 text-gray-600 dark:text-white  py-3 rounded-full w-full outline-none focus:ring-1"
-                       placeholder="Search ...">
-            </div>
-        </header>
-
-        <section>
-            <nav class="flex space-x-6 text-gray-400 font-medium">
-                <a href="#" class="hover:text-gray-700 dark:hover:text-white">TV Series</a>
-                <a href="#" class="text-gray-700 dark:text-white font-semibold">Movies</a>
-                <a href="#" class="hover:text-gray-700 dark:hover:text-white ">Animes</a>
-            </nav>
-        </section>
-
-        <section class="mt-9">
+        <section class="mt-2">
             <div class="mx-auto w-full max-w-[880px] bg-white shadow-xl rounded-lg p-6">
                 <form action="/createEvent" method="post" enctype="multipart/form-data">
                     @csrf
@@ -143,7 +110,7 @@
                                 </label>
                                 <select name="country"
                                         class="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
-                                        >
+                                >
                                     <option value="" selected>Choose a Country</option>
                                     @foreach($data as $item)
                                         <option value="{{ $item }}">{{ $item }}</option>
@@ -159,7 +126,7 @@
                                 </label>
                                 <input type="text" name="city" placeholder="City"
                                        class="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
-                                       />
+                                />
                             </div>
                         </div>
                     </div>
@@ -240,7 +207,7 @@
                     </script>
                     <div>
                         <button type="submit"
-                            class="hover:shadow-form rounded-md bg-black py-3 px-8 text-center text-base font-semibold text-white outline-none">
+                                class="hover:shadow-form rounded-md bg-black py-3 px-8 text-center text-base font-semibold text-white outline-none">
                             Submit
                         </button>
                     </div>
@@ -251,7 +218,7 @@
     </main>
 
     <!-- Right Sidebar -->
-{{--    @include('organizer.right_side')--}}
+    {{--    @include('organizer.right_side')--}}
     <!-- /Right Sidebar -->
 </div>
 </body>
